bda775c2ff3e59a1e697d197972e793683b59be1
nombre <daniel.b100726@gmail.com>
2021-08-19 16:52:59 -0400

    Conexión a MongoAtlas una instancia de MongoDB

---
+++ movies-api/lib/mongo.js
@@ -0,0 +1,34 @@
+const { MongoClient, ObjectId } = require('mongodb');
+const { config } = require('../config');
+
+const USER = encodeURIComponent(config.dbuser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const DB_NAME = config.dbName;
+
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${DB_NAME}?retryWrites=true&w=majority`;
+
+class MongoLib {
+  constructor() {
+    this.client = new MongoClient(MONGO_URI, { useNewUrlParser: true });
+    this.dbName = DB_NAME;
+  }
+
+  connect() {
+    //connection no es una variable que hayamos declarado antes, funciona como una variable estática
+    if (!MongoLib.connection) {
+      MongoLib.connection = new Promise((resolve, reject) => {
+        this.client.connect((error) => {
+          if (error) {
+            reject(error);
+          }
+          console.log('Connected succesfully to mongo');
+          resolve(this.client.db(this.dbName));
+        });
+      });
+    }
+
+    return MongoLib.connection;
+  }
+}
+
+module.exports = MongoLib;
