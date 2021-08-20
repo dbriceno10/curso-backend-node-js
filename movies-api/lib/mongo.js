1bdf035d6e488af51a9b621f6b15c8dbda91de5f
nombre <daniel.b100726@gmail.com>
2021-08-19 23:03:23 -0400

    Clase 27: Implementación de las acciones de MongoDB

--- movies-api/lib/mongo.js
+++ movies-api/lib/mongo.js
@@ -1,11 +1,16 @@
 const { MongoClient, ObjectId } = require('mongodb');
 const { config } = require('../config');
 
+// aquí vamos a crear las diferentes constantes
+// encodeURIComponent nos garantizá que si por alguna razón hay algunos caracteres especiales
+// no tengamos problemas a la hora de conectarnos.
+
 const USER = encodeURIComponent(config.dbuser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const DB_NAME = config.dbName;
 
 const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${DB_NAME}?retryWrites=true&w=majority`;
+// mongodb+srv://db_user_platzivideos:<password>@cluster0-nnl4g.mongodb.net/test?retryWrites=true&w=majority
 
 class MongoLib {
   constructor() {
@@ -28,6 +33,50 @@ class MongoLib {
     }
 
     return MongoLib.connection;
+  }
+
+  //Creando nuestra implementación de acciones
+  getAll(collection, query) {
+    //Nos va a traer todos los elementos de la conección
+    return this.connect().then((db) => {
+      return db.collection(collection).find(query).toArray();
+    });
+  }
+
+  get(collection, id) {
+    //nos va a traer un único elemento de la conección
+    return this.connect().then((db) => {
+      return db.collection(collection).findOne({ _id: ObjectId(id) });
+    });
+  }
+
+  create(collection, data) {
+    //nos permite agregar datos
+    return this.connect()
+      .then((db) => {
+        return db.collection(collection).insertOne(data);
+      })
+      .then((result) => result.insertedId);
+  }
+
+  update(collection, id, data) {
+    //permite actualizar un dato en específico
+    return this.connect()
+      .then((db) => {
+        return db
+          .collection(collection)
+          .updateOne({ _id: ObjectId(id), $set: data }, { upsert: true });
+      })
+      .then((result) => result.upsertedId || id);
+  }
+
+  delete(collection, id) {
+    //nos permite borrar un dato en específico
+    return this.connect()
+      .then((db) => {
+        return db.collection(collection).deleteOne({ _id: ObjectId(id) });
+      })
+      .then(() => id);
   }
 }
 
